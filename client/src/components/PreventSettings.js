c719eade8b8a5b6cae18a4e6f517571374eea54d
davidalexandervega <davidalexandervega@gmail.com>
2022-10-11 14:31:47 -0700

    prevent the demo user from accessing settings

---
+++ client/src/components/PreventSettings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PreventSettings = () => {
+  return (
+    <div className="preventSettings">
+      settings are disabled for the demo user. however, you're free to edit the profile.
+    </div>
+  );
+};
+
+export default PreventSettings;
